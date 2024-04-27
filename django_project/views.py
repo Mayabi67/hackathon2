e12fcc739e1c3ffc85aff64b92d9bc41d39b5d78
Mayabi67 <160654772+Mayabi67@users.noreply.github.com>
2024-04-27 02:28:21 +0000

    first commit

--- django_project/views.py
+++ django_project/views.py
@@ -2,11 +2,22 @@ import requests
 from django.shortcuts import render
 
 
+def fetch_data(url):
+  try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad status codes
+    return response.json()
+  except requests.exceptions.RequestException as e:
+    print(f"Error fetching data from {url}: {e}")
+    return None
+
+
 def home(request):
   #USING APIs => Example 1
-  response = requests.get('https://api.github.com/events')
+  response = requests.get(
+      'https://restcountries.com/v3.1/all?fields=name,flags`')
   data = response.json()
-  results = data[0]["repo"]
+  results = data[0:1]
 
   #Example 2
   response2 = requests.get('https://dog.ceo/api/breeds/image/random')
@@ -14,12 +25,27 @@ def home(request):
   results2 = data2["message"]
 
   #Example 3
-  response3 = requests.get('https://api.github.com/users/octocat/repos')
+  response3 = requests.get('http://randomfox.ca/floof/')
   data3 = response3.json()
-  results3 = data3[0]["name"]
+  results3 = data3["image"]
+                           
 
-  return render(request, 'templates/index.html', {
-      'results': results,
-      'results2': results2,
-      'results3': results3
-  })
+  #Example 4
+  response4 = requests.get(
+      'https://uselessfacts.jsph.pl/random.json?language=en')
+  data4 = response4.json()
+  results4 = data4["text"]
+
+  #Example 5
+  response5 = requests.get('https://api.chucknorris.io/jokes/random')
+  data5 = response5.json()
+  results5 = data5["value"]
+
+  return render(
+      request, 'templates/index.html', {
+          'results': results,
+          'results2': results2,
+          'results3': results3,
+          'results4': results4,
+          'results5': results5
+      })
